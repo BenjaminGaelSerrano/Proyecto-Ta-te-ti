ea9a0a747341a649621b6f40da0c4c77370afbf0
BenjaminGaelSerrano <alumno26.serrano.benjamin@ipm.edu.ar>
2025-07-11 13:35:40 -0300

    toma pa vos san

--- codigos/pruebas/fotopyserial.py
+++ codigos/pruebas/fotopyserial.py
@@ -1,50 +1,46 @@
 import serial
 import cv2
 import os
-import time 
-# Ruta personalizada (puedes cambiarla)
-ruta_guardado = "/home/ta-te-ti/Escritorio/Tateti/Proyecto-Ta-te-ti/imagenes/pygamefoto1.jpg"
+import time
 
-# Asegúrate de que la carpeta exista
+
+from analIsis import detectar_rojo_en_imagen
+
+# Ruta personalizada
+ruta_guardado = "/home/ta-te-ti/Escritorio/Tateti/Proyecto-Ta-te-ti/imagenes/pygamefoto1.jpg"
 os.makedirs(os.path.dirname(ruta_guardado), exist_ok=True)
 
-# Abrir la cámara
+# Abrir cámara
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("No se pudo abrir la cámara.")
     exit()   
 
-
-
-# Cambia 'COM3' por el puerto de tu Arduino.
-# En Linux probablemente sea '/dev/ttyUSB0' o '/dev/ttyACM0'
+# Conexión serial
 puerto = '/dev/ttyUSB0'
 baudrate = 9600
-
-# Abre la conexión serial
 ser = serial.Serial(puerto, baudrate, timeout=1)
 
 print("Escuchando datos del Arduino...")
 
 try:
     while True:
-        # Lee una línea del puerto serial
         ret, frame = cap.read()
-        if ser.in_waiting>0:
+        if ser.in_waiting > 0:
             linea = ser.readline().decode('utf-8').strip()
-            
-            print(f"Arduino dice:{linea}")
+            print(f"Arduino dice: {linea}")
+
             if ret:
                 cv2.imwrite(ruta_guardado, frame)
-                print(f"Foto guardada en: {ruta_guardado}") 
+                print(f"📸 Foto guardada en: {ruta_guardado}")
+
+               
+                detectar_rojo_en_imagen(ruta_guardado)
             else:
-                print("No se pudo capturar la imagen")
-            
-                
+                print("❌ No se pudo capturar la imagen")
 
 except KeyboardInterrupt:
     print("Programa terminado por el usuario.")
 finally:
     ser.close()
-
-cap.release()
+    cap.release()
